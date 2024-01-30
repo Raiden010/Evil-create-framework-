44ad3b5dfaad003eb2ea108125bcf683ea2982fa
Retr0 <119252533+Raiden010@users.noreply.github.com>
2024-01-30 00:20:14 -0400

    Update windows.py

--- documents /windows.py
+++ documents /windows.py
@@ -1,1 +1,23 @@
+R = '\033[31m' # Red
+N = '\033[1;37m' # White
+def a():
+        print ""+N+""
+        print "VIRUS FOR WINDOWS COLLECTIONS"
+        print "============================="
+        print
+        print "name                                 rank"
+        print "----                                 ----\033[31m"
+        print "modules/windows/module_req_eater     normal"
+        print "module/windows/module_quis           normal"
+        print "module/windows/module_ugly           normal"
+        print "module/windows/module_sleepy         normal"
+        print "module/windows/module_rip            normal"
+        print "module/windows/module_koce           normal"
+        print "module/windows/module_capslock       funny :v"
+        print "module/windows/module_rdc            normal"
+        print "module/windows/module_alay           normal"
+        print "module/windows/module_zipbomb        low"
+        print "module/windows/module_cmd            normal"
 
+  if __name__ == "__main__":
+          a()
